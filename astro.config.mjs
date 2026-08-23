788c0ae9a21301b1418ee0d7cd564d6186cd4117
simplifi-git <klara.calabro@simplifi.solutions>
2026-08-23 22:36:55 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://simplifi-git.github.io',
-  base: '/Website-for-simplifi.solutions',
-  integrations: [sitemap()],
-  output: 'static'
+  site: 'https://simplifi.solutions',
 });
