d21502e699fa31b55c03cfb6bf8f6b4d346ae0cc
simplifi-git <klara.calabro@simplifi.solutions>
2026-08-30 08:48:42 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://simplifi.solutions',
+  integrations: [sitemap()],
   build: {
     assets: 'assets'
   }
